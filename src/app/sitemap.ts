fa7176098be82dfd2b91e0177a07c92de533f02d
Aliyanasif <150242892+Aliyanasif@users.noreply.github.com>
2026-05-13 15:46:54 +0500

    Add blog pages to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -28,5 +28,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 0.4,
     },
+    {
+        url: `${baseUrl}/blog`,
+        lastModified: new Date(),
+        changeFrequency: "weekly",
+        priority: 0.8,
+      },
+      {
+        url: `${baseUrl}/blog/best-attendance-management-software-pakistan`,
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.75,
+      },
   ];
 }
